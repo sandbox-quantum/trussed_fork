2556302f1ef67e2b3ca6629190d6a7c1bb90d0a8
Duc Nguyen <ductri.nguyen@sandboxquantum.com>
2024-03-07 14:53:12 -0500

    rename test

--- tests/kyber.rs
+++ tests/kyber.rs
@@ -2,7 +2,7 @@
 
 use std::println;
 
-use trussed::client::mechanisms::Kyber768;
+use trussed::client::mechanisms::{Kyber1024, Kyber512, Kyber768};
 use trussed::client::CryptoClient;
 use trussed::types::{KeySerialization, Mechanism, StorageAttributes};
 use trussed::{syscall, try_syscall};
@@ -12,9 +12,36 @@ mod client;
 use trussed::types::Location::*;
 
 #[test]
+fn kyber512() {
+    client::get(|client| {
+        let (sk, pk) = syscall!(client.generate_kyber512_keypair(Internal, Volatile));
+
+        let (ciphertext, secret1) = syscall!(client.encap_kyber512(pk, Volatile));
+
+        let secret2 = syscall!(client.decap_kyber512(sk, ciphertext, Volatile));
+
+        // Trussed® won't give out secrets, but lets us use them
+        let derivative1 = syscall!(client.sign_hmacsha256(secret1, &[])).signature;
+        let derivative2 = syscall!(client.sign_hmacsha256(secret2, &[])).signature;
+        assert_neq!(derivative1, derivative2);
+        println!("{:?}", derivative1);
+        println!("{:?}", derivative2);
+
+        assert!(try_syscall!(client.serialize_key(
+            Mechanism::SharedSecret,
+            secret1,
+            KeySerialization::Raw
+        ))
+        .is_err());
+        let _ =
+            syscall!(client.serialize_key(Mechanism::SharedSecret, secret2, KeySerialization::Raw));
+    })
+}
+
+#[test]
 fn kyber768() {
     client::get(|client| {
-        let (sk, pk) = syscall!(client.generate_kyber768_keypair(Volatile));
+        let (sk, pk) = syscall!(client.generate_kyber768_keypair(Internal, Volatile));
 
         let (ciphertext, secret1) = syscall!(client.encap_kyber768(pk, Volatile));
 
@@ -39,18 +66,20 @@ fn kyber768() {
 }
 
 #[test]
-fn kyber768_internal() {
+fn kyber1024() {
     client::get(|client| {
-        let (sk, pk) = syscall!(client.generate_kyber768_keypair(Internal));
+        let (sk, pk) = syscall!(client.generate_kyber1024_keypair(Internal, Volatile));
 
-        let (ciphertext, secret1) = syscall!(client.encap_kyber768(pk, Internal));
+        let (ciphertext, secret1) = syscall!(client.encap_kyber1024(pk, Volatile));
 
-        let secret2 = syscall!(client.decap_kyber768(sk, ciphertext, Internal));
+        let secret2 = syscall!(client.decap_kyber1024(sk, ciphertext, Volatile));
 
         // Trussed® won't give out secrets, but lets us use them
         let derivative1 = syscall!(client.sign_hmacsha256(secret1, &[])).signature;
         let derivative2 = syscall!(client.sign_hmacsha256(secret2, &[])).signature;
-        assert_eq!(derivative1, derivative2);
+        assert_neq!(derivative1, derivative2);
+        println!("{:?}", derivative1);
+        println!("{:?}", derivative2);
 
         assert!(try_syscall!(client.serialize_key(
             Mechanism::SharedSecret,
@@ -62,4 +91,3 @@ fn kyber768_internal() {
             syscall!(client.serialize_key(Mechanism::SharedSecret, secret2, KeySerialization::Raw));
     })
 }
-
